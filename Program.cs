be2cce868be06d9b14b827ff4cb217672f7d8f08
ckomalram <glaw14@gmail.com>
2023-03-23 23:13:23 -0500

    ++ Base de datos en memoria

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyef.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//configurando general  EntityFramework core - Base de dato en memoria
+builder.Services.AddDbContext<TareaContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/dbconexion", async ([FromServices] TareaContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("DB en memoria " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
