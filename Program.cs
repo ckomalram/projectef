23b7b996260d5eef6e0874b2efcc11ce1fa8445e
ckomalram <glaw14@gmail.com>
2023-03-23 23:34:40 -0500

    ++ Conf a sql server

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 //configurando general  EntityFramework core - Base de dato en memoria
-builder.Services.AddDbContext<TareaContext>(p => p.UseInMemoryDatabase("TareasDB"));
+// builder.Services.AddDbContext<TareaContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
+// conf EF para sql server
+// ICARLYLE\SQLEXPRESS
+// "Data Source=SERVIDOR;Initial Catalog=BASE_DE_DATOS;User ID=USUARIO;Password=CONTRASEÑA;"
+
+builder.Services.AddSqlServer<TareaContext>("Data Source=ICARLYLE\\SQLEXPRESS;Initial Catalog=TareasDb;User ID=sa;Password=sa;");
 
 var app = builder.Build();
 
