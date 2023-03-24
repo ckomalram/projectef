5ef478e87c7aa527a6f5a470811e5d08a169a154
ckomalram <glaw14@gmail.com>
2023-03-24 11:35:23 -0500

    ++ Crear Datos

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using proyef.Context;
+using proyef.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,15 +24,31 @@ app.MapGet("/dbconexion", async ([FromServices] TareaContext dbContext) =>
     return Results.Ok("DB en memoria " + dbContext.Database.IsInMemory());
 });
 
-app.MapGet("/api/tareas",  async ( [FromServices] TareaContext dbContext) =>
+app.MapGet("/api/tareas", async ([FromServices] TareaContext dbContext) =>
 {
     //  return Results.Ok(dbContext.Tareas.Where(p => p.PrioridadTarea == proyef.Models.Prioridad.Baja ) );
-    //  return Results.Ok(dbContext.Tareas );
+     return Results.Ok(dbContext.Tareas );
     //  return Results.Ok(dbContext.Tareas.Include(p => p.Categoria));
-     return Results.Ok(dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == proyef.Models.Prioridad.Baja ) );
+    // return Results.Ok(dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == proyef.Models.Prioridad.Baja));
 
 
 });
+
+app.MapPost("/api/tareas", async ([FromServices] TareaContext dbContext, [FromBody] Tarea tarea) =>
+{
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreado = DateTime.Now;
+    // opcion 1
+    await dbContext.AddAsync(tarea);
+
+    // opcion 2
+    // await dbContext.Tareas.AddAsync(tarea);
+
+    await dbContext.SaveChangesAsync();
+    return Results.Ok();
+
+});
+
 
 
 app.Run();
